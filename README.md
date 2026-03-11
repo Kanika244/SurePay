1ffb88cfc4b91a88a0da0f26285439157a989a28
Parth Sharma <154320618+parthsh17@users.noreply.github.com>
2026-03-11 11:20:51 +0530

    fix: server.js ES module compatibility

---
+++ README.md
@@ -0,0 +1,3 @@
+https://surepay-backend-5aa957c407df.herokuapp.com/ 
+
+https://surepay-frontend-1ce34bb911bf.herokuapp.com/
--- frontend/server.js
+++ frontend/server.js
@@ -2,6 +2,13 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('*', (req, res) => {
